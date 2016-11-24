e6d7ec5852fbb8edbc9b9a863d77398da3280da8
Luciano Ramalho <luciano@ramalho.org>
2016-11-23 23:43:20 -0200

    mais alguns testes

--- runefinder.go
+++ runefinder.go
@@ -1,121 +1,137 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"path"
-	"strconv"
-	"strings"
-	"time"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "path"
+    "strconv"
+    "strings"
+    "time"
 )
 
 const ucdFileName = "UnicodeData.txt"
 const ucdBaseUrl = "http://www.unicode.org/Public/UCD/latest/ucd/"
 
 func progressDisplay(running <-chan bool) {
-	for {
-		select {
-		case <-running:
-			fmt.Println("xxx")
-		case <-time.After(200 * time.Millisecond):
-			fmt.Print(".")
-		}
-	}
+    for {
+        select {
+        case <-running:
+            fmt.Println("xxx")
+        case <-time.After(200 * time.Millisecond):
+            fmt.Print(".")
+        }
+    }
 }
 
 func getUcdFile(fileName string) {
 
-	url := ucdBaseUrl + ucdFileName
-	fmt.Printf("%s not found\nretrieving from %s\n", ucdFileName, url)
-	running := make(chan bool)
-	go progressDisplay(running)
-	defer func() {
-		running <- false
-	}()
-	response, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
-	file, err := os.Create(fileName)
-	if err != nil {
-		panic(err)
-	}
-	_, err = io.Copy(file, response.Body)
-	if err != nil {
-		panic(err)
-	}
-	file.Close()
+    url := ucdBaseUrl + ucdFileName
+    fmt.Printf("%s not found\nretrieving from %s\n", ucdFileName, url)
+    running := make(chan bool)
+    go progressDisplay(running)
+    defer func() {
+        running <- false
+    }()
+    response, err := http.Get(url)
+    if err != nil {
+        panic(err)
+    }
+    defer response.Body.Close()
+    file, err := os.Create(fileName)
+    if err != nil {
+        panic(err)
+    }
+    _, err = io.Copy(file, response.Body)
+    if err != nil {
+        panic(err)
+    }
+    file.Close()
+}
+
+func tokenize(texts ...string) []string {
+    tokens := []string{}
+    for _, text := range texts {
+        for _, word := range strings.Split(text, " ") {
+            for _, token := range strings.Split(word, "-") {
+                tokens = append(tokens, token)
+            }
+        }
+    }
+    return tokens
+}
+
+func indexWords(text string) (map[string][]rune, map[rune]string) {
+    lines := strings.Split(text, "\n")
+
+    index := map[string][]rune{}
+    names := map[rune]string{}
+
+    for _, line := range lines {
+        var uchar rune
+        fields := strings.Split(line, ";")
+        if len(fields) >= 2 {
+            code64, _ := strconv.ParseInt(fields[0], 16, 0)
+            uchar = rune(code64)
+            names[uchar] = fields[1]
+            // fmt.Printf("%#v", index)
+            for _, word := range tokenize(fields[1], fields[10]) {
+                var entries []rune
+                if len(index[word]) < 1 {
+                    entries = make([]rune, 0)
+                } else {
+                    entries = index[word]
+                }
+                index[word] = append(entries, uchar)
+            }
+        }
+
+    }
+    return index, names
 }
 
 func buildIndex(fileName string) (map[string][]rune, map[rune]string) {
-	if _, err := os.Stat(fileName); os.IsNotExist(err) {
-		getUcdFile(fileName)
-	}
-	content, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		panic(err)
-	}
+    if _, err := os.Stat(fileName); os.IsNotExist(err) {
+        getUcdFile(fileName)
+    }
+    content, err := ioutil.ReadFile(fileName)
+    if err != nil {
+        panic(err)
+    }
 
-	lines := strings.Split(string(content), "\n")
-
-	index := map[string][]rune{}
-	names := map[rune]string{}
-
-	for _, line := range lines {
-		var uchar rune
-		fields := strings.Split(line, ";")
-		if len(fields) >= 2 {
-			code64, _ := strconv.ParseInt(fields[0], 16, 0)
-			uchar = rune(code64)
-			names[uchar] = fields[1]
-			// fmt.Printf("%#v", index)
-			for _, word := range strings.Split(fields[1], " ") {
-				var entries []rune
-				if len(index[word]) < 1 {
-					entries = make([]rune, 0)
-				} else {
-					entries = index[word]
-				}
-				index[word] = append(entries, uchar)
-			}
-		}
-
-	}
-	return index, names
+    return indexWords(string(content))
 }
 
 func findRunes(query string, index map[string][]rune) []rune {
-	found := []rune{}
-	for _, uchar := range index[strings.ToUpper(query)] {
-		found = append(found, uchar)
-	}
-	return found
+    found := []rune{}
+    for _, uchar := range index[strings.ToUpper(query)] {
+        found = append(found, uchar)
+    }
+    return found
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage:  runefinder <word>\texample: runefinder cat")
-		os.Exit(1)
-	}
-	word := os.Args[1]
+    if len(os.Args) != 2 {
+        fmt.Println("Usage:  runefinder <word>\texample: runefinder cat")
+        os.Exit(1)
+    }
+    word := os.Args[1]
 
-	dir, _ := os.Getwd()
-	path := path.Join(dir, ucdFileName)
-	index, names := buildIndex(path)
+    dir, _ := os.Getwd()
+    path := path.Join(dir, ucdFileName)
+    index, names := buildIndex(path)
 
-	count := 0
-	format := "U+%04X  %c \t%s\n"
-	for _, uchar := range findRunes(word, index) {
-		if uchar > 0xFFFF {
-			format = "U+%5X %c \t%s\n"
-		}
-		fmt.Printf(format, uchar, uchar, names[uchar])
-		count++
-	}
-	fmt.Printf("%d characters found\n", count)
+    count := 0
+    format := "U+%04X  %c \t%s\n"
+    for _, uchar := range findRunes(word, index) {
+        if uchar > 0xFFFF {
+            format = "U+%5X %c \t%s\n"
+        }
+        fmt.Printf(format, uchar, uchar, names[uchar])
+        count++
+    }
+    fmt.Printf("%d characters found\n", count)
 
 }
--- runefinder_test.go
+++ runefinder_test.go
@@ -13,7 +13,7 @@ type MySuite struct{}
 
 var _ = check.Suite(&MySuite{})
 
-func (s *MySuite) TestFindRunes(c *check.C) {
+func (s *MySuite) TestFindOneWord(c *check.C) {
 	index := map[string][]rune{
 		"REGISTERED": []rune{0xAE},
 	}
@@ -21,6 +21,29 @@ func (s *MySuite) TestFindRunes(c *check.C) {
 	tests := map[string][]rune{
 		"registered": []rune{0xAE},
 		"nonesuch":   []rune{},
+	}
+	for query, found := range tests {
+		c.Assert(findRunes(query, index), check.DeepEquals, found)
+	}
+}
+
+func (s *MySuite) TestIndexHyphenatedWord(c *check.C) {
+	index, _ := indexWords("002D;HYPHEN-MINUS;Pd;0;ES;;;;;N;;;;;")
+
+	tests := map[string][]rune{
+		"hyphen": []rune{0x2D},
+	}
+	for query, found := range tests {
+		c.Assert(findRunes(query, index), check.DeepEquals, found)
+	}
+}
+
+func (s *MySuite) TestIndexOldNameField(c *check.C) {
+	index, _ := indexWords(
+		"0028;LEFT PARENTHESIS;Ps;0;ON;;;;;Y;OPENING PARENTHESIS;;;;")
+
+	tests := map[string][]rune{
+		"opening": []rune{0x28},
 	}
 	for query, found := range tests {
 		c.Assert(findRunes(query, index), check.DeepEquals, found)
