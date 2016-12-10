f11663b15625c7759196351ad4b79eaa83fa23f3
Luciano Ramalho <luciano@ramalho.org>
2016-12-10 18:45:24 -0200

    started main func

--- runefinder.go
+++ runefinder.go
@@ -3,6 +3,9 @@ package main
 import (
 	"strconv"
 	"strings"
+	"bufio"
+	"os"
+	"fmt"
 )
 
 func tokenize(s string) []string {
@@ -12,7 +15,7 @@ func tokenize(s string) []string {
 	return strings.FieldsFunc(s, separator)
 }
 
-func stringInSlice(needle string, haystack []string) bool {
+func sliceHasString(haystack []string, needle string) bool {
 	for _, item := range haystack {
 		if item == needle {
 			return true
@@ -21,19 +24,46 @@ func stringInSlice(needle string, haystack []string) bool {
 	return false
 }
 
+func sliceHasAllStrings(haystack []string, needles []string) bool {
+	for _, needle := range needles {
+		if !sliceHasString(haystack, needle) {
+			return false
+		}
+	}
+	return true
+}
+
 // Parse takes a line from the UCD (Unicode Character Database) text
 // and returns: the rune, words from the name and legacy name
-func Parse(ucdLine string) (rune, []string) {
+func Parse(ucdLine string) (rune, string, []string) {
 	fields := strings.Split(ucdLine, ";")
 	code64, _ := strconv.ParseInt(fields[0], 16, 0)
 	uchar := rune(code64)
-	words := tokenize(fields[1])
+	name := fields[1]
+	words := tokenize(name)
 	if len(fields[10]) > 0 {
+		appended := false
 		for _, word := range tokenize(fields[10]) {
-			if !stringInSlice(word, words) {
+			if !sliceHasString(words, word) {
 				words = append(words, word)
+				appended = true
 			}
 		}
+		if appended {
+			name += " | " + fields[10]
+		}
 	}
-	return uchar, words
+	return uchar, name, words
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		uchar, name, words := Parse(input.Text())
+		fmt.Println(uchar)
+		if sliceHasAllStrings(words, os.Args[1:]) {
+			fmt.Printf("%c\t%s", uchar, name)
+		}
+
+	}
 }
--- runefinder_test.go
+++ runefinder_test.go
@@ -26,17 +26,41 @@ func TestParse(t *testing.T) {
 	var tests = []struct {
 		input     string
 		wantChar  rune
+		wantName  string
 		wantWords []string
 	}{
-		{"0026;AMPERSAND;Po;0;ON;;;;;N;;;;;", '&', []string{"AMPERSAND"}},
-		{"0021;EXCLAMATION MARK;Po;0;ON;;;;;N;;;;;", '!', []string{"EXCLAMATION", "MARK"}},
-		{"002E;FULL STOP;Po;0;CS;;;;;N;PERIOD;;;;", '.', []string{"FULL", "STOP", "PERIOD"}},
-		{"0027;APOSTROPHE;Po;0;ON;;;;;N;APOSTROPHE-QUOTE;;;", '\'', []string{"APOSTROPHE", "QUOTE"}},
+		{"0026;AMPERSAND;Po;0;ON;;;;;N;;;;;",
+			'&', "AMPERSAND", []string{"AMPERSAND"}},
+		{"0021;EXCLAMATION MARK;Po;0;ON;;;;;N;;;;;",
+			'!', "EXCLAMATION MARK", []string{"EXCLAMATION", "MARK"}},
+		{"002E;FULL STOP;Po;0;CS;;;;;N;PERIOD;;;;",
+			'.', "FULL STOP | PERIOD", []string{"FULL", "STOP", "PERIOD"}},
+		{"0027;APOSTROPHE;Po;0;ON;;;;;N;APOSTROPHE-QUOTE;;;",
+			'\'', "APOSTROPHE | APOSTROPHE-QUOTE", []string{"APOSTROPHE", "QUOTE"}},
 	}
 	for _, test := range tests {
-		if gotChar, gotWords := Parse(test.input); gotChar != test.wantChar ||
-			!reflect.DeepEqual(gotWords, test.wantWords) {
-			t.Errorf("Parse(%q) = (%v, %v)", test.input, gotChar, gotWords)
+		if gotChar, gotName, gotWords := Parse(test.input); gotChar != test.wantChar ||
+				gotName != test.wantName ||
+				!reflect.DeepEqual(gotWords, test.wantWords) {
+			t.Errorf("Parse(%q) = (%v, %v, %v)", test.input, gotChar, gotName, gotWords)
+		}
+	}
+}
+
+func TestSliceHasAllStrings(t *testing.T) {
+	var tests = []struct {
+		inHaystack []string
+		inNeedles  []string
+		want  		 bool
+	}{
+		{[]string{"PLUS", "SIGN"}, []string{"SIGN", "PLUS"}, true},
+		{[]string{"PLUS", "SIGN"}, []string{"SIGN"}, true},
+		{[]string{"PLUS", "SIGN"}, []string{"SPAM"}, false},
+		{[]string{"PLUS", "SIGN"}, []string{"PLUS", "SPAM"}, false},
+	}
+	for _, test := range tests {
+		if got := sliceHasAllStrings(test.inHaystack, test.inNeedles); got != test.want {
+			t.Errorf("siceHasAllStrings(%q, %q) = %v", test.inHaystack, test.inNeedles, got)
 		}
 	}
 }
